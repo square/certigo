75971b0d3ff8023ecd328e9d5975643730266a3a
Cedric Staub <cs@squareup.com>
2016-07-27 14:20:20 -0700

    Prepare for 1.3.0 release

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ var fileExtToFormat = map[string]string{
 }
 
 func main() {
-	app.Version("1.2.0")
+	app.Version("1.3.0")
 
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 	case dump.FullCommand(): // Dump certificate
